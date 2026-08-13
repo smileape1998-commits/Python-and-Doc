ba120a16383508adebe8ff77377fe9695001b20c
APIVIT YIN. <apivit.y@takeitsolution.com>
2026-08-13 17:48:43 +0700

    refactor: remove legacy automation scripts and initialize new course lecture and lab Jupyter notebooks

--- run_py.ps1
+++
@@ -1,5 +0,0 @@
-$pyFile = "c:\Users\apiwi\Downloads\Python and Doc\Python and Doc\update_sec_5_1.py"
-$pyCode = [System.IO.File]::ReadAllText($pyFile, [System.Text.Encoding]::UTF8)
-
-# Execute via powershell python call or python script execution
-powershell -Command "python '$pyFile'"
